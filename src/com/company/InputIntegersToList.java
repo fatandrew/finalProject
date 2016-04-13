f8e7f3980c1ce2638d2f1e34b40743590fd40169
Andrew Danylenko <wdanylenko@gmail.com>
2016-04-14 00:25:05 +0300

    add

--- src/com/company/InputIntegersToList.java
+++ src/com/company/InputIntegersToList.java
@@ -1,13 +1,15 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import com.sun.xml.internal.fastinfoset.util.StringIntMap;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.LinkedList;
 
 public class InputIntegersToList {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        LinkedList<Integer> list = new LinkedList<Integer>();
         System.out.println("Enter numbers please, when it will be enough enter a char: ");
 
 
@@ -19,7 +21,23 @@ public class InputIntegersToList {
         for(int i = 0; i < nums.length; i++){
             System.out.println(nums[i]);
         }
+        System.out.println("List of numbers: " +list);
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter one more num");
+
+            list.addFirst(scanner.nextInt());
+
+
+        System.out.println("List of numbers: " +list);
+
+        Scanner scanner1=new Scanner(System.in);
+        System.out.println("Enter one more num");
+
+        list.addLast(scanner1.nextInt());
+
 
         System.out.println("List of numbers: " +list);
     }
+
 }
